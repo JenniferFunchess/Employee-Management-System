8b440833144006f87a502855aaf07698869121dd
Jennifer Funchess <jfunchess@ymail.com>
2020-12-29 13:59:43 -0600

    set up runTracker function

---
+++ index.js
@@ -0,0 +1,72 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+
+const util = require("util");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employees_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+
+  manageEmployees();
+});
+
+connection.query = util.promisify(connection.query);
+
+function userOptions() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selection",
+        message: "What would you like to do?",
+        choices: [
+          "Add a new Department",
+          "Add a new Role",
+          "Add a new Employee",
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Update an Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then(({ selection }) => {
+      switch (selection) {
+        case "Add a new Department":
+          addDept();
+          break;
+        case "Add a new Role":
+          addRole();
+          break;
+        case "Add a new Employee":
+          addEmployee();
+          break;
+        case "View All Departments":
+          viewAllDept();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "Update an Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          exit();
+        default:
+          console.log("Have a great day!");
+      }
+    });
+}
